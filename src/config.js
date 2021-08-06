b69f3b6cbe55c320134691b3d957b3fc0e664bf4
Daniel Egli <daniel@egli.ca>
2021-08-06 11:12:52 -0700

    PDEV-2443: Replace deprecated crypto cipher functions

--- src/config.js
+++ src/config.js
@@ -165,7 +165,6 @@ function load(callback) {
       const env = fs.readFileSync(envPath, { encoding: 'utf8' });
 
       // Replace the database password with the encrypted value.
-      // const newEnv = env.replace(res.source.password, `ENC:${encrypted}`);
       const newEnv = env.replace(/(source_password(?:\s*)=(?:\s*))(.*)/g, `$1ENC:${encrypted}`);
 
       // Write the new .env file content
--- src/password.js
+++ src/password.js
@@ -1,18 +1,36 @@
 const crypto = require('crypto');
 
-function encrypt(cleartext, password) {
-  const cipher = crypto.createCipher('aes256', password);
+const algorithm = 'AES-256-CBC';
+
+/*
+  Function encrypts a cleartext string in the format:
+    ENC:IV:encryptedstring
+      - 'ENC' indicates the string is encrypted and can be discarded
+      - 'IV' is the random initialization vector required for decryption
+      - 'encryptedstring' is the actual encrypted value
+*/
+function encrypt(cleartext, secret) {
+  // Confirm that secret is 32 characters
+  if (!secret.length === 32) {
+    throw new Error(`Secret "${secret}" is not 32 characters`);
+  }
+
+  // Create random initialization vector.  Will be appended to encrypted string
+  // for decryption usage
+  const iv = crypto.randomBytes(16).toString('hex').slice(0, 16);
+  const cipher = crypto.createCipheriv(algorithm, secret, iv);
   let encrypted = cipher.update(cleartext, 'utf8', 'hex');
   encrypted += cipher.final('hex');
-
-  return encrypted;
+  return `${iv.toString('hex')}|${encrypted}`;
 }
 
-function decrypt(encrypted, password) {
-  const decipher = crypto.createDecipher('aes256', password);
-  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted;
+function decrypt(encrypted, secret) {
+  const textParts = encrypted.split('|');
+  const iv = textParts[0];
+  const encryptedText = textParts[1];
+  const decipher = crypto.createDecipheriv(algorithm, secret, iv);
+  const decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+  return (decrypted + decipher.final('utf8'));
 }
 
 module.exports = {
