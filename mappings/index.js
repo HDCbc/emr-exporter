d72e5d372825ec91d84fa2d67e371b44ffa292cb
jzacharuk <jzacharuk@gmail.com>
2024-08-28 11:17:11 -0700

    PDEV-4512: Require mappings/preprocessors (#67)

---
+++ mappings/index.js
@@ -0,0 +1,7 @@
+const oscar = require('./oscar.json');
+const mois = require('./mois.json');
+
+module.exports = {
+  oscar,
+  mois,
+};
---
+++ preprocessors/index.js
@@ -0,0 +1,5 @@
+const oscar = require('./oscar.json');
+
+module.exports = {
+  oscar,
+};
